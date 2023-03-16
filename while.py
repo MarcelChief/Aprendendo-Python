ce61c205d4bdb349b1ac41c97b06e212da0da468
Marcel de Morais <106990727+MarcelChief@users.noreply.github.com>
2023-03-15 22:24:13 -0300

    testando a estrutura while

---
+++ while.py
@@ -0,0 +1,22 @@
+
+verificar = input('Digite ai as coisas: ')
+verificar_so_letra = verificar.isnumeric()
+
+
+while verificar_so_letra == True:
+    print('Meus parabéns')
+    verificar = input('Digite ai as coisas: ')
+
+
+# um programa onde o usuário vai cadastrando produtos em uma lista 
+
+
+#venda = input('Digite aqui as compras que você fará: ')
+#vendas = []
+
+
+#while venda != '':
+#    vendas.append(venda)
+#    venda = input('Digite aqui as compras que você fará: ')
+
+#print('Suas compras foram {}' .format(vendas))
